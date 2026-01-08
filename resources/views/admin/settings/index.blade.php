75890b92cc3094b8b06a32b32e291d6d54bddc9f
Adil1501 <ben.adil1501@gmail.com>
2026-01-08 16:07:37 +0100

    Project documentatie (README) toegevoegd

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -171,11 +171,11 @@
 
                     <div>
                         <label class="block text-sm font-medium text-gray-700 mb-2">
-                            KvK / Ondernemingsnummer
+                            Ondernemingsnummer
                         </label>
                         <input type="text"
-                               name="business_kvk"
-                               value="{{ old('business_kvk', $settings['business_kvk'] ?? '') }}"
+                               name="business_ondnr"
+                               value="{{ old('business_ondnr', $settings['business_ondnr'] ?? '') }}"
                                class="w-full px-4 py-2 border border-gray-300 rounded-lg"
                                placeholder="0123.456.789">
                     </div>
